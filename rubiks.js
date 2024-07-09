dcabe82af6e753a819ce946f0ee4f5a7f459245c
Lisa <140429827+lsc9919@users.noreply.github.com>
2024-07-09 19:40:09 +0000

    "deleted old code + added more randomness"

--- rubiks.js
+++ rubiks.js
@@ -1,6 +1,6 @@
 //sets parameters for the overall canvas
-const width = 120;
-const height = 120;
+const width = 150;
+const height = 150;
 
 setDocDimensions(width, height);
 
@@ -8,19 +8,15 @@ setDocDimensions(width, height);
 const colors = ["Blue", "Green", "White", "Red", "Orange", "Yellow", "Magenta", "Purple", "Pink", "LightBlue", "OrangeRed", "LightGreen", "YellowGreen", "Teal",  "#cc66ff", "#ff99ff",
 "#0066ff", "#0099ff", "#ccffff"]
 
-const finalLines = [];
 
 // parameters for the size/location of the cube
-let xStart = 30
-let yStart = 50;
-let xEnd = 70;
-let yEnd = 90;
-let interval = bt.randIntInRange(1, 45);
+let xStart = bt.randIntInRange(10, 100);
+let yStart = bt.randIntInRange(30, 80);
+let xEnd = bt.randIntInRange(20, (120-interval));
+let yEnd = bt.randIntInRange(90, 110);
+let interval = bt.randIntInRange(1, 35);
 
-const polyline = [
-  [xStart, yEnd],
-  [yEnd, yEnd],
-];
+
 
 // handles each baby square in the cube
 function inner(x, i,y){
@@ -33,17 +29,12 @@ function inner(x, i,y){
   return innerLines;
 }
 
-finalLines.push(polyline);
-
-// outer layer of the cube
-drawLines(finalLines, {width: 15 });
-
 // draws the inside cubies and randomizes the colors of each piece
 for(let i = xStart; i<=xEnd; i=i+interval){
   for(let j = yStart;j<=yEnd;j=j+interval){
     // changed randomness
     const random = Math.floor(Math.random() * colors.length);
     let color = colors[random]
-    drawLines(inner(i,interval,j), { fill: color, width: 15 });
+    drawLines(inner(i,interval,j), { fill: color, width: 5 });
   }
 }
